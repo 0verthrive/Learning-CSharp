1e438a097a93399dddafe9976508fcf6adc91156
Overthrive <sara.leal.lessa@gmail.com>
2021-12-12 23:04:20 -0300

    Adicionando condicional

--- ProjetosCSNelio/PrimeiroPrograma/FirstConsole/FirstConsole/Program.cs
+++ ProjetosCSNelio/PrimeiroPrograma/FirstConsole/FirstConsole/Program.cs
@@ -16,6 +16,10 @@ namespace FirstConsole
                 Console.WriteLine("2 - Encontrando a área do circulo");
                 Console.WriteLine("3 - Diferença entre produtos");
                 Console.WriteLine("4 - Calculo salarial");
+                Console.WriteLine("5 - Verificando sinal");
+                Console.WriteLine("6 - Par ou ímpar");
+                Console.WriteLine("7 - Múltiplos");
+                Console.WriteLine("8 - Duração do jogo");
                 Console.WriteLine("0 - Sair");
                 opc = int.Parse(Console.ReadLine());
 
@@ -67,6 +71,77 @@ namespace FirstConsole
                         double salario = porHora * horasTrab;
 
                         Console.WriteLine("Matricula: {0}\nSalário a receber = {1:F2}", matricula, salario);
+
+                        break;
+                    case 5 :
+                        Console.WriteLine("Informe um valor: ");
+                        double comparativo = double.Parse(Console.ReadLine());
+
+                        if(comparativo < 0)
+                        {
+                            Console.WriteLine("O valor {0} é negativo", comparativo);
+                        }
+                        else
+                        {
+                            Console.WriteLine("O valor {0} é positivo", comparativo);
+                        }
+                        break;
+                    case 6 :
+                        Console.WriteLine("Informe um valor: ");
+                        double aux = double.Parse(Console.ReadLine());
+                        if(aux %2 == 0)
+                        {
+                            Console.WriteLine("Valor {0} é par", aux);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Valor {0} é ímpar", aux);
+                        }
+                        break;
+                    case 7 :
+                        Console.WriteLine("Informe dois números inteiros: ");
+                        int num1 = int.Parse(Console.ReadLine());
+                        int num2 = int.Parse(Console.ReadLine());
+                        float result;
+                        int auxiliar;
+
+                        if(num1 > num2) 
+                        { 
+                            result = (float)num1/num2;
+                        }
+                        else
+                        {
+                            result = (float)num2/num1;
+                        }
+
+                        auxiliar = (int)result;
+
+                        if(auxiliar == result)
+                        {
+                            Console.WriteLine("São múltiplos");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Não são múltiplos");
+                        }
+                        break;
+                    case 8 :
+                        Console.WriteLine("Informe a hora inicial: ");
+                        int inicio = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Informe a hora final: ");
+                        int fim = int.Parse(Console.ReadLine());
+                        int duracao;
+
+                        if(inicio < fim)
+                        {
+                            duracao = inicio - fim;
+                        }
+                        else
+                        {
+                            duracao = 24 - inicio + fim;
+                        }
+
+                        Console.WriteLine("O jogo durou: {0}h", duracao);
 
                         break;
                 }
